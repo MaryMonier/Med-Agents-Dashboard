7335a914252d8ab452b92e400f50e53bdc299cf5
mary <marymonier94@gmail.com>
2026-05-26 16:27:35 +0300

    initial dashboard structure

---
+++ src/app/prescriptions/prescriptions-module.ts
@@ -0,0 +1,10 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { PrescriptionsRoutingModule } from './prescriptions-routing-module';
+
+@NgModule({
+  declarations: [],
+  imports: [CommonModule, PrescriptionsRoutingModule],
+})
+export class PrescriptionsModule {}
