40d6fb4c3561cdd4741ef04c4d023c89b5d20742
ShazaElsayedIsmail11 <elsayedshaza9@gmail.com>
2026-06-01 15:13:56 +0300

    Add dashboard route
    - Restored dashboard route after adding prescription routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,10 +2,13 @@ import { Routes } from '@angular/router';
 import { Login } from './auth/login/login';
 import { PrescriptionsList } from './prescriptions/prescriptions-list';
 import { PrescriptionForm } from './prescriptions/prescription-form';
+import { Home } from './dashboard/home/home';
 
 export const routes: Routes = [
   { path: '', redirectTo: 'login', pathMatch: 'full' },
   { path: 'login', component: Login },
+
+  { path: 'dashboard', component: Home },
 
   { path: 'prescriptions', component: PrescriptionsList },
   { path: 'prescriptions/new', component: PrescriptionForm },
