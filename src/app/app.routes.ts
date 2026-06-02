0593774049e03dd3bd3e08ed595fb1b242343edf
saraashour040-tech <saraashour040@gmail.com>
2026-06-02 05:03:41 +0300

    add consultations route to app routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,7 +2,13 @@ import { Routes } from '@angular/router';
 import { Login } from './auth/login/login';
 
 export const routes: Routes = [
-    {path:'', redirectTo:'login' ,pathMatch: 'full'},
-    {path:'login', component :Login},
-    {path: '**', redirectTo: 'login'}
+    { path: '', redirectTo: 'login', pathMatch: 'full' },
+    { path: 'login', component: Login },
+    {
+        path: 'consultations',
+        loadChildren: () =>
+            import('./consultations/consultations-module')
+                .then(m => m.ConsultationsModule)
+    },
+    { path: '**', redirectTo: 'login' }
 ];
