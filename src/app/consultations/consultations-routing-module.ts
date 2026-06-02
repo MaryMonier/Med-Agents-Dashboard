6c22720d5a61cc030258721de0debf44163c4bd2
saraashour040-tech <saraashour040@gmail.com>
2026-06-02 04:57:13 +0300

    update consultations routing module

--- src/app/consultations/consultations-routing-module.ts
+++ src/app/consultations/consultations-routing-module.ts
@@ -1,10 +1,29 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    loadComponent: () =>
+      import('./consultation-list/consultation-list')
+        .then(m => m.ConsultationListComponent)
+  },
+  {
+    path: 'add',
+    loadComponent: () =>
+      import('./consultation-form/consultation-form')
+        .then(m => m.ConsultationFormComponent)
+  },
+  {
+    path: 'edit/:id',
+    loadComponent: () =>
+      import('./consultation-form/consultation-form')
+        .then(m => m.ConsultationFormComponent)
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule],
+  exports: [RouterModule]
 })
 export class ConsultationsRoutingModule {}
