d18a4a0595675ddf975a04e2ba206809d6db08bd
saraashour040-tech <saraashour040@gmail.com>
2026-06-10 14:43:36 +0300

    Revert " add ngx-translate localization setup"
    This reverts commit b50a70f40b512c06e20a0ff4d51708806ce75f80.

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,30 +1,13 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners, importProvidersFrom } from '@angular/core';
+import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { provideRouter } from '@angular/router';
-import { provideHttpClient, HttpClient } from '@angular/common/http';
-import { TranslateHttpLoader } from '@ngx-translate/http-loader';
+import { provideHttpClient } from '@angular/common/http';
 
 import { routes } from './app.routes';
-import { TranslateModule, TranslateLoader } from '@ngx-translate/core';
-
-export function HttpLoaderFactory(http: HttpClient) {
-  return new TranslateHttpLoader(http);
-}
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideRouter(routes),
     provideHttpClient(),
-
-    importProvidersFrom(
-      TranslateModule.forRoot({
-        defaultLanguage: 'en',
-        loader: {
-          provide: TranslateLoader,
-          useFactory: HttpLoaderFactory,
-          deps: [HttpClient]
-        }
-      })
-    )
   ]
 };
